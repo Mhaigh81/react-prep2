86a3b9a1aad54d86fa560f305b6427c94bc57e84
Mhaigh81 <mhaigh@me.com>
2026-02-01 13:06:04 -0500

    first

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+function Posts() {
+    const { id } = useParams();
+    
+    useEffect(() => {
+        async function fetchPosts() {
+            const data = await axios.get("https://jsonplaceholder.typicode.com/posts/1")   
+            console.log(data)   
+        }
+        fetchPosts()    
+    },[])
+    
+    return (
+        <div>
+            {id}
+        </div>
+    )
+}
+
+export default Posts
