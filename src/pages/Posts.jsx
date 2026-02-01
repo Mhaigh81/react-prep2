a355b0c6d4a98fd1190e7e88f8e0493a187391fa
Mhaigh81 <mhaigh@me.com>
2026-02-01 15:50:50 -0500

    cm

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -25,42 +25,42 @@ const Posts = () => {
     },[])
         
         return (
-        <>
+          <>
             <div className="post__search">
-                <button>← Back</button>
-                <div className="post__search--container">
-                    <label className="post__search--label">Search by Id</label>
-                    <input 
-                        type="number" 
-                        value={searchId} 
-                        onChange={(event) => setSearchId(event.target.value)}
-                        onKeyPress={(event) => {
-                            if(event.key === "Enter") {
-                            onSearch()
-                            }
-                        }} 
-                        />    
-                    <button onClick={() => onSearch()}>Enter</button>
-                </div>
+              <button>← Back</button>
+              <div className="post__search--container">
+                <label className="post__search--label">Search by Id</label>
+                <input
+                  type="number"
+                  value={searchId}
+                  onChange={(event) => setSearchId(event.target.value)}
+                  onKeyPress={(event) => {
+                    if (event.key === "Enter") {
+                      onSearch();
+                    }
+                  }}
+                />
+                <button onClick={() => onSearch()}>Enter</button>
+              </div>
             </div>
-            {loading 
-            ? new Array(10).fill(0).map((_, index) => (
-                <div className="post" key={index}>
+            {loading
+              ? new Array(10).fill(0).map((_, index) => (
+                  <div className="post" key={index}>
                     <div className="post__title">
-                    <div className="post__title--skeleton"></div>
-                </div>
-                <div className="post__body">
-                    <p className="post__body--skeleton"></p>
-                </div>
-                </div>
-                ))
-                : posts.map(post => (
-                    <div className="post" key={post.id}>
-                        <div className="post__title">{post.title}</div>
-                        <p className="post__body">{post.body}</p>
+                      <div className="post__title--skeleton"></div>
                     </div>
+                    <div className="post__body">
+                      <p className="post__body--skeleton"></p>
+                    </div>
+                  </div>
+                ))
+              : posts.map((post) => (
+                  <div className="post" key={post.id}>
+                    <div className="post__title">{post.title}</div>
+                    <p className="post__body">{post.body}</p>
+                  </div>
                 ))}
-        </>
+          </>
         );
 }
 
